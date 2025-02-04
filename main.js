69683444875bbc8485b907ddc65179e0faf8b064
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-02-04 11:34:33 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+// let ism = prompt("ismingizni kriting");
+// console.log(ism);
+
+// var eski
+// let zori
+// const o'zgartirib bo'lmaydi
+
+// let num1 = +prompt("1 raqam kriting");
+// let num2 = +prompt("2 raqam kriting");
+
+//1string
+//2number
+//bolian
+//underfide
+//null
+//symble
+//begint
+//object
+var string = prompt("string kriting");
+console.log(string);
+
+
+var number = +prompt("number kriting");
+console.log(number);
+
+var bolian = prompt("bolian kriting");
+console.log(bolian);
+
+var togrimi = prompt("hama malumot togrimi");
+console.log(togrimi);
+
+
+
+
+
+
+
+
+
+alert(`stringi tegishli:${string}
+    numberi tegishli:${number}
+    boliani tegishli:${bolian}`);
